174f82d8238173d0e7b7160a97ffa97840f419dc
pavan6cs <bhavanas904@gmail.com>
2023-09-30 15:57:14 +0530

    added input style

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import email from "../assets/email.svg";
 import back_arrow from "../assets/back_arrow.svg";
-import browseFile from "../assets/browseFile.svg";
 import uploadFile from "../assets/upload_file.svg";
-import validateFile from "../assets/validating_file.svg";
+import loader from "../assets/validating_file.svg";
+
 import Modal from "./Modal";
 
 const Form = () => {
@@ -11,6 +11,7 @@ const Form = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [loading, setLoading] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const inputRef = useRef();
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
@@ -24,38 +25,29 @@ const Form = () => {
 
     setLoading(true);
 
-
-      reader.onload = (e) => {
-        try {
-          console.log("helo")
-          const jsonData = JSON.parse(e.target.result);
-          console.log(jsonData);
-
-          setTimeout(()=>{
-            setJsonContent(JSON.stringify(jsonData, null, 2));
-            setLoading(false)
-           
-          },1000)
-          setErrorMessage(" ");
-         
-        
-        } catch (error) {
-          setJsonContent("");
+    reader.onload = (e) => {
+      try {
+       
+        const jsonData = JSON.parse(e.target.result);
+        setTimeout(() => {
+          setJsonContent(JSON.stringify(jsonData, null, 2));
           setLoading(false);
-          setErrorMessage("Invalid JSON file. Please upload a valid JSON file.");
-        }
-      };
-
-   
+        }, 1000);
+        setErrorMessage(" ");
+      } catch (error) {
+        setJsonContent("");
+        setLoading(false);
+        setErrorMessage("Invalid JSON file. Please upload a valid JSON file.");
+      }
+    };
 
     reader.readAsText(file);
   };
 
-const submitHandler =(e)=>{
-e.preventDefault();
-setShowModal(true)
-}
-
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setShowModal(true);
+  };
 
   return (
     <>
@@ -74,41 +66,41 @@ setShowModal(true)
         </div>
 
         <label> Upload JSON file</label>
-        <div>
-        <input type="file"
-        className="input__file"
-        onChange={handleFileChange} />
-       <img src={uploadFile} alt="" />
+        <div className="file__container">
+          <input
+            type="file"
+            ref={inputRef}
+            className="input__file"
+            onChange={handleFileChange}
+          />
+
+         
+          {loading ?  <img className="loader__image" src={loader} />
+        :    <img
+        src={uploadFile}
+        alt="upload file"
+        onClick={() => inputRef.current.click()}
+      />
+        }
         </div>
-     
-
-
-        {/* <div className="file__container">
-        <img src={browseFile} alt="browse file image" 
-        className="upload__file--img"
-        />
-        <p>Browse file</p>
-        </div> */}
 
         {errorMessage && <div className="error__message">{errorMessage}</div>}
-
-{loading && <p>Loading...</p>}
 
         <label>File Contents</label>
         <textarea cols="20" rows="8" value={jsonContent} readOnly></textarea>
 
         <div className="btn__container">
           <button
-   
-          className="submit__btn"
-          // disabled
-           type="submit">
+            className="submit__btn"
+            disabled={!jsonContent}
+            type="submit"
+          >
             Submit
           </button>
         </div>
       </form>
 
-      {showModal &&  <Modal setShowModal={setShowModal} />}
+      {showModal && <Modal setShowModal={setShowModal} />}
     </>
   );
 };
